82048d1ac451b9aa14d42053537be669f345707f
ccnmagnoo <crcamposn@gmail.com>
2024-10-15 12:36:52 -0300

    extract only pdf files from dir

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 
+from utils.get_all_pdf import get_all_pdf
 from utils.text_process import pdf_to_text, text_to_file
-source = "../../../crcam/OneDrive - Ministerio de Energia/Proyectos Públicos/PV Museo HN Valparaíso/consumos"
-target = 'content'
+SOURCE = "../../../crcam/OneDrive - Ministerio de Energia/Proyectos Públicos/PV Museo HN Valparaíso/consumos"
+TARGET = 'content'
 # source = "../../../crcam/Downloads/mhnv"
 
 
-scan = os.scandir(path=source)
+scan = get_all_pdf(source=SOURCE)
 filename = list(scan)[0]
-filepath = source+'/'+filename.name
 
 
-content = pdf_to_text(source_path=filepath)
-text_to_file(output_filepath=source+'/'+target+'/'+filename.name+'.txt',content=content)
+
+content = pdf_to_text(source_path=SOURCE+'/'+filename.name)
+text_to_file(output_filepath=SOURCE+'/'+TARGET+'/'+filename.name+'.txt',content=content)
