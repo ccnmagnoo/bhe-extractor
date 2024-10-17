1ee0798f6bbda09acba3037c8e62bbdfd63f30fc
ccnmagnoo <crcamposn@gmail.com>
2024-10-17 15:08:30 -0300

    invoice dataclass in replace to dict pattern

--- models/DataPattern.py
+++ models/DataPattern.py
@@ -1,13 +1,23 @@
+from dataclasses import dataclass
 from re import Pattern, compile
 
 #cspell: disable
 type PatternModel = dict[str,Pattern[str]]
 
-chilquinta = {
-    'cliente':compile(r'(\d{5,6}-\d)'),
-    'medidor': compile(r'(\d{7,8})EmpresaActual'),
-    'lectura_actual': compile(r'EmpresaActual\s*(\d+)\s*kWh'),
-    'lectura_anterior': compile(r'Anterior\s*-\s*(\d+)\s*kWh'),
-    'reactiva_actual':compile(r'EmpresaActual\s*(\d+)\s*Kvarh'),
-    'reactiva_anterior': compile(r'Anterior\s*-\s*(\d+)\s*Kvarh'),
-    }
+@dataclass
+class InvoicePattern:
+    cliente:Pattern[str]
+    medidor:Pattern[str]
+    lectura_act:Pattern[str]
+    lectura_ant:Pattern[str]
+    reactiva_act:Pattern[str]
+    reactiva_ant:Pattern[str]
+
+chilquinta = InvoicePattern(
+    cliente=compile(r'(\d{5,6}-\d)'),
+    medidor= compile(r'(\d{7,8})EmpresaActual'),
+    lectura_act= compile(r'EmpresaActual\s*(\d+)\s*kWh'),
+    lectura_ant= compile(r'Anterior\s*-\s*(\d+)\s*kWh'),
+    reactiva_act=compile(r'EmpresaActual\s*(\d+)\s*Kvarh'),
+    reactiva_ant= compile(r'Anterior\s*-\s*(\d+)\s*Kvarh'),
+)
--- utils/data_process.py
+++ utils/data_process.py
@@ -1,20 +1,25 @@
+from dataclasses import fields
 from os import DirEntry
-from models.DataPattern import PatternModel,chilquinta
+from re import Pattern
+from models.DataPattern import InvoicePattern, PatternModel,chilquinta
 
 
 def process_content_files(source:list[DirEntry[str]],pattern_model:PatternModel):
     return None
 
-def text_to_data(file:DirEntry[str],patter_model:PatternModel):
+def text_to_data(file:DirEntry[str],patter_model:InvoicePattern):
     "extract data from txt file"
     print('processing string content in:',file)
 
 
     with open(file,'rb') as f:
+        
         content = str(f.read(),encoding='ansi')
         print('file:',file.name)
         
-        for key,compiler in patter_model.items():
+        for field in fields(patter_model):
+            key:str = field.name
+            compiler:Pattern[str] = getattr(patter_model,key)
             result = compiler.findall(content)
             print('result for',key,'is',result)
         f.close()
