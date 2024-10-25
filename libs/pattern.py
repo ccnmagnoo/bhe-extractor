88cf39e42213ee869cc33eefcfc21add7a73c5e8
ccnmagnoo <crcamposn@gmail.com>
2024-10-25 09:22:52 -0300

    update datetime data convert

---
+++ libs/pattern.py
@@ -0,0 +1,23 @@
+from re import Pattern, compile
+from models.DataPattern import InvoiceAdapter
+
+#cspell: disable
+
+chilquinta = InvoiceAdapter[Pattern](
+    client=compile(r'(\d{5,6}-\d)'),
+    measurer= compile(r'(\d{7,8})EmpresaActual'),
+    fare= compile(r'tarifa contratada: ([\w*-]*\s?\w*)'),
+    period=compile(r'Monto del per[i,í]odo: (\d{2} \w{3} \d{4}) - (\d{2} \w{3} \d{4})'),
+    lecture_act= compile(r'EmpresaActual\s*(\d+)\s*kWh'),
+    lecture_ant= compile(r'Anterior\s*-\s*(\d+)\s*kWh'),
+    reactive_act=compile(r'EmpresaActual\s*(\d+)\s*Kvarh'),
+    reactive_ant= compile(r'Anterior\s*-\s*(\d+)\s*Kvarh'),
+    power_demand=compile(r'Demanda Leida :\s+(\d*\W?\d)* kW'),
+    electricity_consumption=compile(r'Electricidad consumida (\d+) kWh'),
+    electricity_cost=compile(r'Electricidad consumida \d+ kWh \$ (\d*.?\d+)'),
+    power_max=compile(r'demanda máxima\s+(\d*\W?\d)* kW'),
+    power_max_cost=compile(r'demanda máxima\s+\d*\W?\d* kW \$ (\d*.?\d+)'),
+    admin_cost=compile(r'servicio \$ (\d*.?\d+)'),
+    transport_cost=compile(r'transporte de electricidad \$ (\d*.?\d+)'),
+    total_bill=compile(r'\d{5,6}-\d \d{2} \w{3} \d{4}\r\n\s*\$ (\d*.?\d+)'),
+)
