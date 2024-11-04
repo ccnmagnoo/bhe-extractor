37a8148684ca50a2e2c092ce3d65f558bbb7c105
ccnmagnoo <crcamposn@gmail.com>
2024-11-04 15:50:41 -0300

    invoice with default values

--- models/DataPattern.py
+++ models/DataPattern.py
@@ -13,41 +13,41 @@ type Transformer[IN,OUT] = Callable[[IN,Callable[[IN],OUT]],OUT]
 @dataclass
 class Invoice:
     """invoice with electric bill"""
-    client:str
-    measurer:int
-    fare:str
-    period:tuple[datetime,datetime]
-    lecture_act:int
-    lecture_ant:int
-    reactive_act:int
-    reactive_ant:int
-    power_demand:float
-    electricity_consumption:float
-    electricity_cost:int
-    power_max:float
-    power_max_cost:int
-    admin_cost:int
-    transport_cost:int
-    total_bill:int
+    client:str = ''
+    measurer:int = 0
+    fare:str = ''
+    period:tuple[datetime,datetime] = []
+    lecture_act:int = 0
+    lecture_ant:int = 0
+    reactive_act:int = 0
+    reactive_ant:int = 0
+    power_demand:float = 0.0
+    electricity_consumption:float = 0.0
+    electricity_cost:int = 0
+    power_max:float = 0.0
+    power_max_cost:int = 0
+    admin_cost:int = 0
+    transport_cost:int = 0
+    total_bill:int = 0
 
 @dataclass
-class InvoiceAdapter[T]:
+class InvoiceAdapter[TY,PERIOD]:
     """Regex Pattern requirements to match certain Invoice param"""
-    client:T
-    measurer:T
-    fare:T
-    period:T
-    lecture_act:T
-    lecture_ant:T
-    reactive_act:T
-    reactive_ant:T
-    power_demand:T
-    electricity_consumption:T
-    electricity_cost:T
-    power_max:T
-    power_max_cost:T
-    admin_cost:T
-    transport_cost:T
-    total_bill:T
+    client:TY
+    measurer:TY
+    fare:TY
+    period:PERIOD
+    lecture_act:TY
+    lecture_ant:TY
+    reactive_act:TY
+    reactive_ant:TY
+    power_demand:TY
+    electricity_consumption:TY
+    electricity_cost:TY
+    power_max:TY
+    power_max_cost:TY
+    admin_cost:TY
+    transport_cost:TY
+    total_bill:TY
 # final
 
